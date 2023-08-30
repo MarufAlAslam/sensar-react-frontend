dab1a2bbd322be98dbece5fbc86028046d8d86c5
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-08-30 23:11:58 +0600

    customer header done

--- src/layouts/main.layout.js
+++ src/layouts/main.layout.js
@@ -8,7 +8,7 @@ const MainLayout = () => {
       <div className="lg:w-[250px] md:w-full sidebar-holder">
         <Sidebar />
       </div>
-      <div className="lg:w-[calc(100%-250px)] md:w-full">
+      <div className="lg:w-[calc(100%-250px)] md:w-full p-[35px] bg-[#f3f4ff] min-h-screen">
         <Outlet />
       </div>
     </div>
--- src/pages/customers/index.js
+++ src/pages/customers/index.js
@@ -1,7 +1,66 @@
 import React from "react";
+import {
+  FaBell,
+  FaCaretDown,
+  FaCog,
+  FaPlus,
+  FaSearch,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="customers">
+      <div className="flex justify-between items-center">
+        <h2 className="text-blue text-3xl font-bold">Customers</h2>
+        <div className="actions text-right flex justify-end items-center gap-8">
+          <button className="btn relative action-btn w-[50px] h-[50px] flex justify-center items-center text-[#a098ae] bg-white rounded-full">
+            <FaBell className="text-2xl" />
+            <div className="status bg-[#4d44b5] w-[8px] h-[8px] rounded-full absolute top-[10px] right-[10px]"></div>
+          </button>
+          <Link
+            to={"/settings"}
+            className="btn relative action-btn w-[50px] h-[50px] flex justify-center items-center text-[#a098ae] bg-white rounded-full"
+          >
+            <FaCog className="text-2xl" />
+          </Link>
+          <Link
+            to={"/profile"}
+            className="flex justify-end gap-4 text-end items-center"
+          >
+            <div className="">
+              <p className="text-blue text-lg font-bold">Nawfel M.</p>
+              <p className="text-sm pinkish">Admin</p>
+            </div>
+            <div className="w-[50px] h-[50px] flex justify-center items-center text-[#a098ae] bg-white rounded-full">
+              <FaUser className="text-2xl" />
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex mt-7 justify-between items-center">
+        <form
+          action=""
+          className="search flex justify-between items-center gap-3 bg-white px-6 py-3 rounded-full"
+        >
+          <FaSearch className="text-blue" />
+          <input type="text" className="w-[300px]" name="" id="" />
+        </form>
+
+        <div className="right text-end flex justify-end items-center gap-7">
+          <p className="text-blue flex justify-start items-center gap-2">
+            Newest <FaCaretDown className="text-xl" />
+          </p>
+
+          <button className="btn-blue flex justify-center items-center gap-2 px-5 py-3">
+            <FaPlus /> <span>Customer</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Customers;
