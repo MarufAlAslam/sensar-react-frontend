c8fa4a704bfb63b862ca6a730585124c1080f1b6
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-10-22 21:17:22 +0600

    subcategory added

--- src/pages/add-new-category/index.js
+++ src/pages/add-new-category/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import React, { useEffect } from "react";
 import { FaBell, FaCog, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const AddNewCategory = () => {
+  const [imgUrl, setImgUrl] = React.useState(null);
+  const [categoryName, setCategoryName] = React.useState(null);
+  const [subCategories, setSubCategories] = React.useState([]);
   const handleAddNewCategory = (e) => {
     e.preventDefault();
 
@@ -52,6 +57,72 @@ const AddNewCategory = () => {
 
   //     console.log(previousData);
   //   };
+  const handleUpdateSubcategory = (e) => {
+    const subcategory = document.getElementById("subcategory").value;
+    setCategoryName(subcategory);
+    const payload = new FormData();
+    payload.append("image", document.getElementById("photo_material").files[0]);
+    // alert(subcategory);
+
+    // get the image from photo_material and upload it to imgbb
+    // get the url of the image from imgbb
+
+    // const photo_material = document.getElementById("photo_material").files[0];
+    axios
+      .post(
+        "https://api.imgbb.com/1/upload?key=bcc56bad76ece711720725d2a509d3dc",
+        payload
+      )
+      .then((response) => {
+        // console.log("response", response);
+        // console.log("response url", response.data.data.image.url);
+        setImgUrl(response.data.data.image.url);
+        // console.log("success");
+      })
+      .catch((error) => {
+        console.log("error", error);
+        alert("try another image agian");
+      });
+
+    // console.log(data);
+
+    // fetch("https://sensar.vercel.app/api/v1/create-subcategory", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     Swal.fire({
+    //       title: "Success!",
+    //       text: "Subcategory has been added successfully!",
+    //       icon: "success",
+    //       confirmButtonText: "Okay",
+    //     });
+    //     // form.reset();
+    //   });
+  };
+  // console.log(imgUrl, categoryName);
+
+  useEffect(() => {
+    const subcategories = JSON.parse(localStorage.getItem("subcategories"));
+    setSubCategories(subcategories);
+  }, []);
+
+  console.log(subCategories);
+  useEffect(() => {
+    setSubCategories([
+      ...subCategories,
+      {
+        categoryName,
+        imgUrl,
+      },
+    ]);
+  }, []);
+
+  console.log(subCategories);
   return (
     <div>
       <div className="md:flex hidden justify-between items-center">
@@ -103,42 +174,44 @@ const AddNewCategory = () => {
             />
           </div>
 
-          {/* <p className="text-blue pt-5 font-bold mb-4">
+          <p className="text-blue pt-5 font-bold mb-4">
             Subcategory (Optional)
           </p>
 
-          <form
-            onSubmit={handleSubCategory}
-            className="flex justify-start items-center gap-4"
-          >
-            <div className="uploader min-w-[140px] w-[140px]">
-              <p className="text-blue font-bold">Photo Material *</p>
+          <div className="flex justify-start items-center gap-4">
+            <div className="photo">
+              <label htmlFor="" className="text-xs text-blue">
+                Photo Material
+              </label>
+              <input type="file" name="photo_material" id="photo_material" />
               <label
-                htmlFor="logo"
-                className="logo-uploader block text-center mt-4 p-2"
+                htmlFor="photo_material"
+                className="file-label text-xs block border border-dashed p-4 mt-3 w-[150px] text-center"
               >
-                <input type="file" name="logo" id="logo" />
-                <span className="text-[10px] text-gray-400">
-                  Drag and drop or click here to select file
-                </span>
+                Drag and drop or click here to select file
               </label>
             </div>
 
-            <div className="w-full">
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded"
-                  placeholder="Melamine"
-                  name="subcategory"
-                  id=""
-                />
-                <button type="submit" className="btn btn-blue rounded-none">
-                  Add
-                </button>
-              </div>
+            <div className="flex border rounded overflow-hidden w-[300px] mt-4">
+              <input
+                type="text"
+                className="w-full p-2 border-r"
+                placeholder="Subcategory"
+                name="subcategory"
+                required
+                id="subcategory"
+              />
+              <button
+                type="button"
+                onClick={handleUpdateSubcategory}
+                className="btn bg-[#4D43B5] text-white px-4 py-2"
+              >
+                Add
+              </button>
             </div>
-          </form> */}
+
+            <div className="flex justify-start items-center gap-2"></div>
+          </div>
 
           <div className="text-right mt-7">
             <button
